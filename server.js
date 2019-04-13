506f31e15ca85659c49cb3623222a4b80a8d9540
JASEpstein <jasepstein@gmail.com>
2019-04-13 11:59:13 -0500

    API hooked up

--- server.js
+++ server.js
@@ -1,31 +1,15 @@
 var express = require("express");
 var path = require("path");
 
-var reservations = require("./data/reservations.js");
-var tables = require("./data/tables.js");
-
 var app = express();
 var PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-app.get("/api/reservations", function(req, res) {
-  return res.json(reservations);
-});
-
-app.post("/api/reservations", function(req, res) {
-  var newReservation = req.body;
-
-  console.log(newReservation);
-
-  reservations.push(newReservation);
-  res.json(newReservation);
-});
+require("./routes/api-routes")(app);
+require("./html-routes")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
-
-console.log(tables);
