50e5d43bd7e3768abc1c81e091377e7224190499
jstevens79 <jstevens79@users.noreply.github.com>
2019-04-13 12:24:23 -0500

    linking things up

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,11 +2,11 @@ var path = require('path');
 
 module.exports = function(app) {
 
-    app.get("/reservations.html", function (req, res) {
+    app.get("/reserve", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/reserve.html"));
     });
 
-    app.get("/tables.html", function (req, res) {
+    app.get("/tables", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/tables.html"));
     });
 
